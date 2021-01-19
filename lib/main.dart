cff8e1d0c422d106bed9a92a0325df43c6111c2a
manirahmanzadeh <mani.rahmanzade@gmail.com>
2021-01-19 22:28:30 +0330

    pop up menu button

--- lib/main.dart
+++ lib/main.dart
@@ -10,58 +10,43 @@ void main(){
 }
 
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin{
-
-  AnimationController animationController;
-  Animation sizeAnimation;
-
-
-
-  @override
-  void initState() {
-    super.initState();
-
-    animationController = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 3)
-    );
-
-    sizeAnimation = Tween(
-      begin: 30.0,
-      end: 300.0
-    ).animate(animationController);
-
-    animationController.addListener(() { setState(() {
-
-    });});
-
-  }
-
-
-
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          width: sizeAnimation.value,
-          height: 100,
-          color: Colors.green,
+        child: PopupMenuButton(
+          itemBuilder: (menuCtx){
+            return [
+              PopupMenuItem(
+                  child: ListTile(
+                    leading: Icon(Icons.message_outlined),
+                    title: Text("some text"),
+                    onTap: (){
+
+                    },
+                  )
+              ),
+              PopupMenuItem(
+                  child: ListTile(
+                    leading: Icon(Icons.check_box_outline_blank),
+
+                    title: Text("some text"),
+                    onTap: (){
+
+                    },
+                  )
+              ),
+            ];
+          },
+          color: Colors.white,
+          child: Icon(Icons.more),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          animationController.forward();
-        },
-      ),
+
     );
   }
 }
+
 
 
