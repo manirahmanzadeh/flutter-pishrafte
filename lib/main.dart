5930d9801a593d3525aaac6bffbf419d0eebac97
manirahmanzadeh <mani.rahmanzade@gmail.com>
2021-01-25 23:50:23 +0330

    tab view

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,46 @@
 
 
-import 'package:flutter_app/splash.dart';
 import 'package:flutter/material.dart';
-
-import 'models/chapter_model.dart';
-import 'pages/flashcards_page.dart';
 
 void main(){
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Fibet",
-      home: SplashPage(),
-      theme: ThemeData(
-        fontFamily: "s",
-        primaryColor: Color(0xFFf48fb1)
-      ),
-    )
+      MaterialApp(
+        home: MyPage()
+      )
   );
 }
 
+class MyPage extends StatelessWidget {
 
-class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("فیبت(فلش کارت های فیزیک)"),
-        bottom: AppBar(
-          title: Text("سرفصل ها"),
-        ),
-      ),
-
-      body: ListView(
-        children: [
-          Wrap(
-            children: chapters.map(
-                    (e) => FlatButton(
-                      padding: EdgeInsets.all(0),
-                        onPressed: (){
-                          Navigator.push(context, MaterialPageRoute(builder: (_)=>FlashCardsPage(chapter:e)));
-                        },
-                        child: Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            Opacity(
-                              opacity: 0.1,
-                              child: Container(
-                                width: (MediaQuery.of(context).size.width-80)/2,
-                                height: (MediaQuery.of(context).size.width-80)/2,
-                                margin: EdgeInsets.all(20),
-                                decoration: BoxDecoration(
-                                  color: Colors.pinkAccent.withOpacity(0.3),
-
-                                  image: DecorationImage(image: AssetImage("assets/splash.png"),fit: BoxFit.cover),
-                                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                                  boxShadow: [
-                                    BoxShadow(
-                                        spreadRadius: 3,
-                                        blurRadius: 3,
-                                        color: Colors.black12
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ),
-                            Text(
-                              e.title,
-                              textAlign: TextAlign.center,
-                              textDirection: TextDirection.rtl,
-                            ),
-                          ],
-                        )
-                    )
-            ).toList(),
-          )
-        ],
-      ),
-
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("my app"),
+            bottom: TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.directions_car)),
+                  Tab(icon: Icon(Icons.directions_transit)),
+                  Tab(icon: Icon(Icons.directions_bike)),
+                ]
+            ),
+          ),
+          body: TabBarView(
+              children: [
+                Icon(Icons.directions_car),
+                Icon(Icons.directions_transit),
+                Icon(Icons.directions_bike),
+              ]
+          ),
+        )
     );
   }
 }
+
+
+
+
+
+
